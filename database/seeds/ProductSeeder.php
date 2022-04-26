29524de8a3e19ea8c396af736984ab420bca3085
pietroscandola <pietro.scandolavr@gmail.com>
2022-04-26 17:20:56 +0200

    add ProductSeeder

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -1,6 +1,14 @@
 <?php
 
+use App\Models\Product;
+use App\Models\Restaurant;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use FakerRestaurant\Restaurant as FakerRestaurant;
+use Illuminate\Support\Arr;
+
+
+
 
 class ProductSeeder extends Seeder
 {
@@ -9,8 +17,20 @@ class ProductSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $foodfaker = \Faker\Factory::create();
+        $foodfaker->addProvider(new \FakerRestaurant\Provider\it_IT\Restaurant($faker));
+
+        $restaurants_ids = Restaurant::pluck('id')->toArray();
+        $categories = ['Primi Piatti', 'Secondi Piatti', 'Bevande', 'Pizze', 'Sushi'];
+        for ($i = 0; $i < 30; $i++) {
+            $product = new Product();
+            $product->restaurant_id = Arr::random($restaurants_ids);
+            $product->name = $foodfaker->foodName();
+            $product->price = $faker->randomFloat(2, 1, 30);
+            $product->category = Arr::random($categories);
+            $product->save();
+        }
     }
 }
