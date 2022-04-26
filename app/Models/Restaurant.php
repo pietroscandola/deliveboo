bfe68197c733ac70bccb972514297b6cd0251653
pietroscandola <pietro.scandolavr@gmail.com>
2022-04-26 16:00:23 +0200

    add Models and add Vue auth

---
+++ app/Models/Restaurant.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Restaurant extends Model
+{
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function products()
+    {
+        return $this->hasMany('App\Models\Product');
+    }
+
+    public function orders()
+    {
+        return $this->hasMany('App\Models\Order');
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany('App\Models\Category');
+    }
+}
--- app/User.php
+++ app/User.php
@@ -10,6 +10,11 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    public function restaurant()
+    {
+        return $this->hasOne('App\Models\Restaurant');
+    }
+
     /**
      * The attributes that are mass assignable.
      *
