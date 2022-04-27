d6b36860fe407d191c39969b962ec6f15456c2b6
vrossi96 <vrossi396@yahoo.it>
2022-04-27 18:34:49 +0200

    show restaurant in home if restaurant is already created

--- resources/views/admin/restaurant/home.blade.php
+++ resources/views/admin/restaurant/home.blade.php
@@ -1,29 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>Benvenuto</h1>
-        <h2>Il tuo ristorante</h2>
+   <div class="container">
+      <h1>Benvenuto</h1>
 
-        {{-- RISTORANTE INFO --}}
+      @if ($restaurant)
+         <h2>Il tuo ristorante</h2>
 
-        <div class="row justify-content-center">
+         {{-- RISTORANTE INFO --}}
+
+         <div class="row justify-content-center">
             <div class="col-12 d-flex justify-content-center">
-                <img src="https://www.zeusnews.it/img/4/8/1/6/2/0/026184-620-google-vedi-immagini.jpg" alt="img">
+               <img src="https://www.zeusnews.it/img/4/8/1/6/2/0/026184-620-google-vedi-immagini.jpg" alt="img">
             </div>
-            <div class="d-flex flex-column">   
-                <h5><strong>Nome:</strong> {{ $restaurant->name }}</h5>
-                <p><strong>Descrizione:</strong> {{ $restaurant->description }}</p>
-                <p><strong>Email:</strong> {{ $restaurant->email }}</p>
-                <p><strong>Cellulare:</strong> {{ $restaurant->phone }}</p>
-                <p><strong>Indirizzo:</strong> {{ $restaurant->address }}</p>
-                <a href="{{ route('admin.restaurants.edit', $restaurant->id) }}" class="btn btn-primary">AA</a>
+            <div class="d-flex flex-column">
+               <h5><strong>Nome:</strong> {{ $restaurant->name }}</h5>
+               <p><strong>Descrizione:</strong> {{ $restaurant->description }}</p>
+               <p><strong>Email:</strong> {{ $restaurant->email }}</p>
+               <p><strong>Cellulare:</strong> {{ $restaurant->phone }}</p>
+               <p><strong>Indirizzo:</strong> {{ $restaurant->address }}</p>
+               <a href="{{ route('admin.restaurants.edit', $restaurant->id) }}" class="btn btn-primary">AA</a>
             </div>
-        </div>
+         </div>
+      @else
+         <h2>Nessun ristorante: aggiungilo</h2>
+      @endif
 
-        {{-- CARD CIBO --}}
+      {{-- CARD CIBO --}}
 
 
 
-    </div>
+   </div>
 @endsection
