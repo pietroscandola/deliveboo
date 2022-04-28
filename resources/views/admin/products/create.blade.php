b618c9e74067071ceaaebe21c2dc74deeaecfc3d
vrossi96 <vrossi396@yahoo.it>
2022-04-28 22:31:03 +0200

    merged forms products

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -1,20 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-   <form action="{{ route('admin.products.store') }}" method="post" enctype="multipart/form-data">
+   @include('admin.products.includes.form')
+   {{-- <form action="{{ route('admin.products.store') }}" method="post" enctype="multipart/form-data">
       @csrf
       <div class="mb-3">
          <label for="name" class="form-label">Nome del Prodotto</label>
          <input type="text" class="form-control" name="name" id="name" value={{ old('name') }}>
       </div>
+
       <div class="mb-3">
          <label for="price" class="form-label">Prezzo</label>
          <input type="number" class="form-control" name="price" id="price" value={{ old('price') }} step="0.01">
       </div>
+
       <div class="mb-3">
          <label for="ingredients" class="form-label">Ingredienti</label>
          <textarea class="form-control" id="ingredients" name="ingredients" rows="3">{{ old('ingredients') }}</textarea>
       </div>
+
       <div class="col-12 mb-3">
          <label for="category">Categoria</label>
          <select class="form-select" id="category" name="category" aria-label="Default select example">
@@ -24,6 +28,7 @@
             @endforeach
          </select>
       </div>
+
       <div class="mb-3 d-flex">
          <div class="col-10">
             <div class="form-group">
@@ -46,7 +51,7 @@
 
       <input type="submit" value="Invia">
 
-   </form>
+   </form> --}}
 @endsection
 
 @section('scripts')
