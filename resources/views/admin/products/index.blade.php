f617612b5ce8364eb1ca0ba71d24ae4861f36f96
pietroscandola <pietro.scandolavr@gmail.com>
2022-04-28 11:38:27 +0200

    add button delete product

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,44 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row align-items-center">
-        <div class="col-12 col-sm-6">
-            <h1>Benvenuto</h1>
-            <h2>I tuoi prodotti</h2>
-        </div>
-        <div class="col-12 col-sm-6 d-flex justify-content-end">
-            <a class="btn btn-secondary" href="{{ route('admin.products.create') }}">Crea</a>
-        </div>
-    </div>
-
-    {{-- RISTORANTE INFO --}}
-
-    <div class="row justify-content-center">
-        {{-- <div class="d-flex flex-wrap"> --}}
-        @foreach ($products as $product)
-        <div class="col-3 m-3">
-            <div class="card" style="width: 18rem;">
-                <img src="{{ $product->image }}" class="card-img-top" alt="{{ $product->name }}">
-                <div class="card-body">
-                    <h5 class="card-title">{{ $product->name }}</h5>
-                    <p class="card-text"><strong>Prezzo:</strong> {{ $product->price }}</p>
-                    <p class="card-text"><strong>Categoria:</strong> {{ $product->category }}</p>
-                    <p class="card-text"><strong>Ingredienti:</strong> {{ $product->ingredients }}</p>
-                    <a href="{{ route('admin.products.show', $product->id) }}" class="btn btn-primary">Vedi</a>
-                    <a href="{{ route('admin.products.edit', $product->id) }}" class="btn btn-warning">Modifica
-                    </a>
-                </div>
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-12 col-sm-6">
+                <h1>Benvenuto</h1>
+                <h2>I tuoi prodotti</h2>
+            </div>
+            <div class="col-12 col-sm-6 d-flex justify-content-end">
+                <a class="btn btn-secondary" href="{{ route('admin.products.create') }}">Crea</a>
             </div>
         </div>
-        @endforeach
-        {{-- </div> --}}
 
+        {{-- RISTORANTE INFO --}}
+
+        <div class="row justify-content-center">
+            {{-- <div class="d-flex flex-wrap"> --}}
+            @foreach ($products as $product)
+                <div class="col-3 m-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{ $product->image }}" class="card-img-top" alt="{{ $product->name }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $product->name }}</h5>
+                            <p class="card-text"><strong>Prezzo:</strong> {{ $product->price }}</p>
+                            <p class="card-text"><strong>Categoria:</strong> {{ $product->category }}</p>
+                            <p class="card-text"><strong>Ingredienti:</strong> {{ $product->ingredients }}</p>
+                            <div class="d-flex justify-content-between">
+                                <a href="{{ route('admin.products.show', $product->id) }}"
+                                    class="btn btn-primary">Vedi</a>
+                                <a href="{{ route('admin.products.edit', $product->id) }}" class="btn btn-warning">
+                                    Modifica
+                                </a>
+                                <form action="{{ route('admin.products.destroy', $product->id) }}" method="post"
+                                    class='delete_form'>
+                                    @csrf
+                                    @method('DELETE')
+                                    <input type="submit" class="btn btn-danger" value="Elimina">
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+            {{-- </div> --}}
+
+        </div>
+        {{-- CARD CIBO --}}
     </div>
+@endsection
 
-    {{-- CARD CIBO --}}
-
-
-
-</div>
+@section('scripts')
+    <script src={{ asset('js/delete-confirm.js') }} defer></script>
 @endsection
