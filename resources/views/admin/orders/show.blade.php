3dc93b91831497f86398c429831a3cc1e9b8a149
AngeloLista <abstractplayer@gmail.com>
2022-04-28 12:02:10 +0200

    -

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -4,15 +4,17 @@
     <div class="col-12">
         <div class="card">
             <div class="card-body">
+                <h5 class="card-text text-center"><strong>ID Ordine: {{ $order->id }}</strong></h5>
+                <hr>
                 <p class="card-text"><strong>Nome:</strong> {{ $order->customer_name }}</p>
-                    <p class="card-text"><strong>Cognome:</strong> {{ $order->customer_surname }}</p>
-                    <p class="card-text"><strong>E-Mail:</strong> {{ $order->customer_email }}</p>
-                    <p class="card-text"><strong>Indirizzo:</strong> {{ $order->customer_address }}</p>
-                    <p class="card-text"><strong>Telefono:</strong> {{ $order->customer_phone }}</p>
-                    <p class="card-text"><strong>Totale Ordine:</strong> {{ $order->amount }}€</p>
-                    <p class="card-text"><strong>Data Creazione Ordine:</strong> {{ $order->created_at }}</p>
-                    <p class="card-text"><strong>Stato Pagamento:</strong>@if($order->is_paid) Pagato @else Non Pagato @endif</p>
-                    <p class="card-text"><strong>Stato Consegna:</strong>@if($order->is_delivered) Consegnato @else Non Cosegnato @endif</p>
+                <p class="card-text"><strong>Cognome:</strong> {{ $order->customer_surname }}</p>
+                <p class="card-text"><strong>E-Mail:</strong> {{ $order->customer_email }}</p>
+                <p class="card-text"><strong>Indirizzo:</strong> {{ $order->customer_address }}</p>
+                <p class="card-text"><strong>Telefono:</strong> {{ $order->customer_phone }}</p>
+                <p class="card-text"><strong>Totale Ordine:</strong> {{ $order->amount }}€</p>
+                <p class="card-text"><strong>Data Creazione Ordine:</strong> {{ $order->created_at }}</p>
+                <p class="card-text"><strong>Stato Pagamento:</strong>@if($order->is_paid) Pagato @else Non Pagato @endif</p>
+                <p class="card-text"><strong>Stato Consegna:</strong>@if($order->is_delivered) Consegnato @else Non Cosegnato @endif</p>
                 <div class="d-flex justify-content-end">
                     <form action="{{ route('admin.orders.destroy', $order->id) }}" method="post">
                         @method('delete')
