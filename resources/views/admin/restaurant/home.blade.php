cf1b997d7c1a72ae62b0e50a09add510da70052a
pietroscandola <pietro.scandolavr@gmail.com>
2022-04-29 10:32:51 +0200

    remove button delete in restaurant home

--- resources/views/admin/restaurant/home.blade.php
+++ resources/views/admin/restaurant/home.blade.php
@@ -1,54 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-   <div class="container">
-      <h1>Benvenuto</h1>
+    <div class="container">
+        <h1>Benvenuto</h1>
 
-      @if ($restaurant)
-         <h3 class="mb-3">Il tuo ristorante</h3>
+        @if ($restaurant)
+            <h3 class="mb-3">Il tuo ristorante</h3>
 
-         {{-- RISTORANTE INFO --}}
+            {{-- RISTORANTE INFO --}}
 
-         <div class="row justify-content-center">
-            <div class="col-xs-12 col-lg-6 d-flex justify-content-center">
-               @if ($restaurant->image)
-                  <img class="img-fluid rounded border" src="{{ asset('storage/' . $restaurant->image) }}"
-                     alt="{{ $restaurant->name }}">
-               @else
-                  <figure>
-                     <img class="img-fluid rounded mb-3 border"
-                        src="https://www.zeusnews.it/img/4/8/1/6/2/0/026184-620-google-vedi-immagini.jpg" alt="img">
-                  </figure>
-               @endif
-            </div>
-            <div class="col-xs-12 col-lg-6 d-flex flex-column justify-content-between">
-               <div class="mb-3">
-                  <h5><strong>Nome:</strong> {{ $restaurant->name }}</h5>
-                  <hr>
-                  <p><strong>Email:</strong> {{ $restaurant->email }}</p>
-                  <p><strong>Cellulare:</strong> {{ $restaurant->phone }}</p>
-                  <p><strong>Indirizzo:</strong> {{ $restaurant->address }}</p>
-                  <p><strong>Descrizione:</strong> {{ $restaurant->description }}</p>
-               </div>
-               {{-- Buttons --}}
-               <div class="d-flex justify-content-end">
-                  <a href="{{ route('admin.restaurants.edit', $restaurant->id) }}"
-                     class="btn btn-primary mr-3">Modifica</a>
-                  <form action="{{ route('admin.restaurants.destroy', $restaurant->id) }}" method="POST">
+            <div class="row justify-content-center">
+                <div class="col-xs-12 col-lg-6 d-flex justify-content-center">
+                    @if ($restaurant->image)
+                        <img class="img-fluid rounded border" src="{{ asset('storage/' . $restaurant->image) }}"
+                            alt="{{ $restaurant->name }}">
+                    @else
+                        <figure>
+                            <img class="img-fluid rounded mb-3 border"
+                                src="https://www.zeusnews.it/img/4/8/1/6/2/0/026184-620-google-vedi-immagini.jpg" alt="img">
+                        </figure>
+                    @endif
+                </div>
+                <div class="col-xs-12 col-lg-6 d-flex flex-column justify-content-between">
+                    <div class="mb-3">
+                        <h5><strong>Nome:</strong> {{ $restaurant->name }}</h5>
+                        <hr>
+                        <p><strong>Email:</strong> {{ $restaurant->email }}</p>
+                        <p><strong>Cellulare:</strong> {{ $restaurant->phone }}</p>
+                        <p><strong>Indirizzo:</strong> {{ $restaurant->address }}</p>
+                        <p><strong>Descrizione:</strong> {{ $restaurant->description }}</p>
+                    </div>
+                    {{-- Buttons --}}
+                    <div class="d-flex justify-content-end">
+                        <a href="{{ route('admin.restaurants.edit', $restaurant->id) }}"
+                            class="btn btn-primary mr-3">Modifica</a>
+                        {{-- <form action="{{ route('admin.restaurants.destroy', $restaurant->id) }}" method="POST">
                      @method('DELETE')
                      @csrf
                      <button type="submit" class="btn btn-danger">Elimina</button>
-                  </form>
-               </div>
+                  </form> --}}
+                    </div>
+                </div>
             </div>
-         </div>
-      @else
-         <h2>Nessun ristorante: <a href="{{ route('admin.restaurants.create') }}">aggiungilo</a></h2>
-      @endif
+        @else
+            <h2>Nessun ristorante: <a href="{{ route('admin.restaurants.create') }}">aggiungilo</a></h2>
+        @endif
 
-      {{-- CARD CIBO --}}
-
+        {{-- CARD CIBO --}}
 
 
-   </div>
+
+    </div>
 @endsection
