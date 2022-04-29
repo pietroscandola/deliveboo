12c21ab4b562e9258f61361d4d92bfb755c28659
vrossi96 <vrossi396@yahoo.it>
2022-04-29 12:56:39 +0200

    add fields with faker in userseeder, and use env files for custom user

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,15 +14,34 @@ class UserSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $user = new User();
-        $user->name = 'Pietro';
-        $user->email = 'pietro@pietro.pietro';
-        $user->password = bcrypt('pietro');
-        $user->save();
+        // #Per cambiare i valori bisogna aggiungere in .env:
+        // USER_NAME=nome che si vuole
+        // USER_SURNAME=cognome che si vuole
+        // USER_EMAIL=mail che si vuole
+
+        if (env('USER_NAME')) {
+            $user = new User();
+            $user->name = env('USER_NAME');
+            $user->surname = env('USER_SURNAME');
+            $user->vat_number = $faker->regexify('[0-9]{11}');
+            $user->email = env('USER_EMAIL');
+            $user->password = bcrypt('admin');
+            $user->save();
+        } else {
+            $user = new User();
+            $user->name = 'admin';
+            $user->surname = 'admin';
+            $user->vat_number = $faker->regexify('[0-9]{11}');
+            $user->email = 'admin@admin';
+            $user->password = bcrypt('admin');
+            $user->save();
+        }
 
         for ($i = 0; $i < 4; $i++) {
             $users = new User();
-            $users->name = $faker->name();
+            $users->name = $faker->firstName();
+            $users->surname = $faker->lastName();
+            $users->vat_number = $faker->regexify('[0-9]{11}');
             $users->email = $faker->email();
             $users->password = bcrypt('password');
             $users->save();
