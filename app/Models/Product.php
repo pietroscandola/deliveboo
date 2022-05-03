cf82a32134220237571cdee57e6b867656a3f955
vrossi96 <vrossi396@yahoo.it>
2022-05-03 14:40:10 +0200

    add soft delete in product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,10 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = ['name', 'price', 'ingredients', 'category', 'image', 'restaurant_id'];
+
     public function restaurant()
     {
         return $this->belongsTo('App\Models\Restaurant');
