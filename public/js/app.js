be4d16113a8ba9045746e51f8bac4a5a67944053
AngeloLista <abstractplayer@gmail.com>
2022-05-04 15:20:58 +0200

    -

--- public/js/app.js
+++ public/js/app.js
@@ -37360,13 +37360,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
 __webpack_require__(/*! C:\Lavavel\deliveboo\resources\js\app.js */"./resources/js/app.js");
 module.exports = __webpack_require__(/*! C:\Lavavel\deliveboo\resources\sass\app.scss */"./resources/sass/app.scss");
-=======
-__webpack_require__(/*! C:\Users\pietr\Desktop\deliveboo\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\pietr\Desktop\deliveboo\resources\sass\app.scss */"./resources/sass/app.scss");
->>>>>>> master
 
 
 /***/ })
