744779a5a22f1508d2c733de7560de80815fa46b
vrossi96 <vrossi396@yahoo.it>
2022-05-09 14:42:32 +0200

    commented scripts in home.blade causing an error in console

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -6,6 +6,6 @@
 
 @section('scripts')
    <script src="{{ asset('js/front.js') }}" defer></script>
-   <script src="https://js.braintreegateway.com/web/3.38.1/js/client.min.js"></script>
-   <script src="https://js.braintreegateway.com/web/3.38.1/js/hosted-fields.min.js"></script>
+   {{-- <script src="https://js.braintreegateway.com/web/3.38.1/js/client.min.js"></script>
+   <script src="https://js.braintreegateway.com/web/3.38.1/js/hosted-fields.min.js"></script> --}}
 @endsection
