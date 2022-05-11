b87d95c6a4cb1481e459516c8b1499258b35c998
pietroscandola <pietro.scandolavr@gmail.com>
2022-05-11 12:09:21 +0200

    add ingredients in product

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -23,15 +23,31 @@ class ProductSeeder extends Seeder
         $foodfaker->addProvider(new \FakerRestaurant\Provider\it_IT\Restaurant($faker));
 
         $restaurants_ids = Restaurant::pluck('id')->toArray();
-        $categories = ['Primi Piatti', 'Secondi Piatti', 'Bevande', 'Pizze', 'Sushi'];
-        $ingredients = 'Pomodoro, mozzarella, pomodoro a fette, mozzarella a fette, olio e origano';
+        $categories = ['Primi Piatti', 'Secondi Piatti', 'Bevande', 'Pizze', 'Sushi', 'Dessert'];
+        $ingredients = [
+            'Pomodoro, mozzarella, pomodoro a fette, mozzarella a fette, olio e origano',
+            'Spaghetti con uovo, pecorino e parmigiano, pancetta, pepe nero',
+            'Risotto cremoso con punte di asparagi',
+            'Straccetti di Manzo con Funghi Misti e Porcini, Olive, Prezzemolo',
+            'Petto di Pollo ai Limoni di Sicilia con Contorno di Patate Rustiche',
+            'Tagliatina di Pollo con Verdure Grigliate e Patate',
+            'Riso Integrale, salmone , avocado, edamame, cavolo rosso',
+            'Riso Bianco, tonno, salmone, melograno, carote, cavolo rosso',
+            'Riso Venere, tofu, avocado, edamame, jelapegnos',
+            'Tenere pepite di pollo croccante',
+            'Patate dalla particolare forma arricciata e dal gusto speziato alla paprika',
+            'Gelato al caffè, al fiordilatte e il nostro gusto tiramisù',
+            'Prodotta con cioccolato Venezuela 70%, caramello e sale di Maldon',
+            'Gnocchi con pesto al basilico, pomodorini e grana padano'
+
+        ];
         for ($i = 0; $i < 120; $i++) {
             $product = new Product();
             $product->restaurant_id = Arr::random($restaurants_ids);
             $product->name = $foodfaker->foodName();
-            $product->price = $faker->randomFloat(2, 1, 20);
+            $product->price = $faker->randomFloat(2, 2, 20);
             $product->category = Arr::random($categories);
-            $product->ingredients = $ingredients;
+            $product->ingredients = Arr::random($ingredients);
             $product->save();
         }
     }
